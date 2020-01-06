38a80bf0113cd08e35c2ee6ef832a740ad6b1314
peternewman22 <peter.newman.22@gmail.com>
2020-01-06 14:31:34 +1100

    Added MoistureSensorTest + initial commit

---
+++ MoistureSensorTest/MoistureSensorTest.ino
@@ -0,0 +1,29 @@
+// Has a relay (default closed) and opens under conditions of the moisture sensor
+
+int sensorPin = A0;
+int relayPin = 2;
+int moistureVal = 0;
+int threshold = 400;
+int interval = 1000;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(sensorPin, INPUT);
+  pinMode(relayPin, OUTPUT);
+  Serial.begin(9600); //init the console writing
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Reading the sensorPin
+  
+  moistureVal = analogRead(sensorPin);
+  Serial.println(moistureVal);
+ 
+  if(moistureVal < threshold){
+    digitalWrite(relayPin, HIGH);
+    } else {
+      digitalWrite(relayPin, LOW);
+      };
+  delay(interval);
+  
+}
